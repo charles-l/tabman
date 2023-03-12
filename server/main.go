421354caa9fc9ff5c92e47c306439d9fae85b66e
charles <charles.saternos@gmail.com>
2023-03-12 17:32:15 -0400

    replace soft delete support with hard deletes (closes #3)

--- server/main.go
+++ server/main.go
@@ -57,106 +57,45 @@ func (server *TabsServer) tabsHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	} else if r.Method == http.MethodGet {
-		if r.URL.Query().Has("deleted") {
-			// deleted sessions
-			rows, err := server.db.Query(`SELECT id, client_id FROM tabs_deleted`)
-			if err != nil {
-				log.Printf("Failed to query tabs_deleted table: %v", err)
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				return
-			}
-			defer rows.Close()
-			var out []map[string]any
-			for rows.Next() {
-				var id int
-				var client_id string
-				rows.Scan(&id, &client_id)
-				out = append(out, map[string]any{
-					"id":        id,
-					"client_id": client_id,
-				})
-			}
-
-			j, err := json.Marshal(out)
-
-			w.Write([]byte(j))
-		} else {
-			// active sessions
-			rows, err := server.db.Query(`SELECT client_id, tabs FROM tabs`)
-			if err != nil {
-				log.Printf("Failed to query tabs table: %v", err)
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				return
-			}
-			defer rows.Close()
-
-			var allTabs []Tabs = []Tabs{}
-			for rows.Next() {
-				var clientId string
-				var tabsStr string
-				if err := rows.Scan(&clientId, &tabsStr); err != nil {
-					log.Printf("failed to scan rows: %v", err)
-					http.Error(w, "Internal error", http.StatusInternalServerError)
-					return
-				}
-
-				var tabsArr [][3]string
-				err := json.Unmarshal([]byte(tabsStr), &tabsArr)
-				if err != nil {
-					// This means we stored bad data, which shouldn't happen.
-					log.Printf("BADNESS: failed to unmarshal rows stored in DB: %v", err)
-					http.Error(w, "Internal error", http.StatusInternalServerError)
-					return
-				}
-				allTabs = append(allTabs, Tabs{clientId, tabsArr})
-			}
-
-			j, err := json.Marshal(allTabs)
-
-			w.Write([]byte(j))
+		// active sessions
+		rows, err := server.db.Query(`SELECT client_id, tabs FROM tabs`)
+		if err != nil {
+			log.Printf("Failed to query tabs table: %v", err)
+			http.Error(w, "Internal error", http.StatusInternalServerError)
+			return
 		}
+		defer rows.Close()
+
+		var allTabs []Tabs = []Tabs{}
+		for rows.Next() {
+			var clientId string
+			var tabsStr string
+			if err := rows.Scan(&clientId, &tabsStr); err != nil {
+				log.Printf("failed to scan rows: %v", err)
+				http.Error(w, "Internal error", http.StatusInternalServerError)
+				return
+			}
+
+			var tabsArr [][3]string
+			err := json.Unmarshal([]byte(tabsStr), &tabsArr)
+			if err != nil {
+				// This means we stored bad data, which shouldn't happen.
+				log.Printf("BADNESS: failed to unmarshal rows stored in DB: %v", err)
+				http.Error(w, "Internal error", http.StatusInternalServerError)
+				return
+			}
+			allTabs = append(allTabs, Tabs{clientId, tabsArr})
+		}
+
+		j, err := json.Marshal(allTabs)
+
+		w.Write([]byte(j))
 	} else if r.Method == http.MethodDelete {
 		id := strings.TrimPrefix(r.URL.EscapedPath(), "/tabs/")
-		log.Printf("attempting to delete '%v'", id)
-		if r.URL.Query().Has("deleted") {
-			// delete from tabs_deleted table
-			if _, err := server.db.Exec("DELETE from tabs_deleted WHERE id = ?", id); err != nil {
-				log.Printf("Error when attempting to delete session: %v", err)
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				return
-			}
-		} else {
-			// move to the tabs_deleted table
-			tx, err := server.db.Begin()
-			if err != nil {
-				log.Printf("Failed to create tx: %v", err)
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				return
-			}
-			var clientId string
-			if err := tx.QueryRow("SELECT client_id FROM tabs WHERE client_id = ?", id).Scan(&clientId); err != nil {
-				tx.Rollback()
-				if err == sql.ErrNoRows {
-					http.Error(w, "Session not found", http.StatusNotFound)
-				} else {
-					log.Printf("Error when attempting to find session: %v", err)
-					http.Error(w, "Internal error", http.StatusInternalServerError)
-				}
-				return
-			}
-			if _, err := tx.Exec("INSERT INTO tabs_deleted (client_id, tabs) SELECT client_id, tabs FROM tabs WHERE client_id = ?", id); err != nil {
-				log.Printf("Query failed: %v", err)
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				tx.Rollback()
-				return
-			}
-			if _, err := tx.Exec("DELETE FROM tabs WHERE client_id = ?", id); err != nil {
-				log.Printf("Query failed: %v", err)
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				tx.Rollback()
-				return
-			}
-			tx.Commit()
+		if _, err := server.db.Exec("DELETE FROM tabs WHERE client_id = ?", id); err != nil {
+			log.Printf("Delete failed: %v", err)
+			http.Error(w, "Internal error", http.StatusInternalServerError)
+			return
 		}
 	}
 }
@@ -176,14 +115,6 @@ func main() {
                 client_id TEXT PRIMARY KEY,
                 tabs JSON,
                 last_updated TEXT
-            );`); err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS tabs_deleted (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                client_id TEXT,
-                tabs JSON
             );`); err != nil {
 		log.Fatal(err)
 	}
